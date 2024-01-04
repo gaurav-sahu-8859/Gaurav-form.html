010655d49fa94898909e66e11a4398cee0e6efc9
gaurav-sahu <127485098+gaurav-sahu-8859@users.noreply.github.com>
2024-01-04 12:50:27 +0530

    updated namrata univarsal
    make a new updated website project using html, css, javascript

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// function inmormateUs(){
+    
+//     var array1 = getElementsByClassName('serviceSection')[0];
+
+//     // const found = array1.find((element) => element > 10);
+//     // alert("you clicked:"+array1)
+//     while(array1 == array1[0]){
+//         alert("you clicked:"+array1)
+//     }
+// }
+function bigImg1(x) {
+     // by using single css property
+    document.getElementById('serviceImg1').style.cssText = "visibility: visible;";
+    // by using multiple css property
+    document.getElementById('serviceSection1').style.cssText = "border: none; height: 240px;";
+    // inmormateUs();
+}
+function normalImg1(x) {
+    document.getElementById('serviceImg1').style.visibility = "hidden";
+    document.getElementById('serviceSection1').style.cssText = "border: 5px solid #131921; height: 200px;";
+}
+
+function bigImg2(x) {
+    document.getElementById('serviceImg2').style.visibility = "visible";
+    document.getElementById('serviceSection2').style.cssText = "border: none; height: 240px;";
+}
+function normalImg2(x) {
+    document.getElementById('serviceImg2').style.visibility = "hidden";
+    document.getElementById('serviceSection2').style.cssText = "border: 5px solid #131921; height: 200px;";
+}
+
+function bigImg3(x) {
+    document.getElementById('serviceImg3').style.visibility = "visible";
+    document.getElementById('serviceSection3').style.cssText = "border: none; height: 240px;";
+}
+function normalImg3(x) {
+    document.getElementById('serviceImg3').style.visibility = "hidden";
+    document.getElementById('serviceSection3').style.cssText = "border: 5px solid #131921; height: 200px;";
+}
+
+function bigImg4(x) {
+    document.getElementById('serviceImg4').style.visibility = "visible";
+    document.getElementById('serviceSection4').style.cssText = "border: none; height: 240px;";
+}
+function normalImg4(x) {
+    document.getElementById('serviceImg4').style.visibility = "hidden";
+    document.getElementById('serviceSection4').style.cssText = "border: 5px solid #131921; height: 200px;";
+}
+
+function bigImg5(x) {
+    document.getElementById('serviceImg5').style.visibility = "visible";
+    document.getElementById('serviceSection5').style.cssText = "border: none; height: 240px;";
+}
+function normalImg5(x) {
+    document.getElementById('serviceImg5').style.visibility = "hidden";
+    document.getElementById('serviceSection5').style.cssText = "border: 5px solid #131921; height: 200px;";
+}
+
+function bigImg6(x) {
+    document.getElementById('serviceImg6').style.visibility = "visible";
+    document.getElementById('serviceSection6').style.cssText = "border: none; height: 240px;";
+}
+function normalImg6(x) {
+    document.getElementById('serviceImg6').style.visibility = "hidden";
+    document.getElementById('serviceSection6').style.cssText = "border: 5px solid #131921; height: 200px;";
+}
